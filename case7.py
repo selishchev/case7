eda3af0607d5d13191519ebd0296b4892eb37a82
selishchev <selishev2010@gmail.com>
2018-12-16 17:07:30 +0300

    commit

---
+++ case7.py
@@ -0,0 +1,37 @@
+"""Case-study #7 Proposal generation
+Developers:
+Selishchev A., Krivosheenkova E., Paymushkin K.,
+
+"""
+
+import random
+
+list_of_symbols = ['"', '#', '$', '%', '&', "'", '(', ')', '*',	'+', '-', '/', ':', ';', '<', '=', '>', '@', '[', ']',
+                   '^', '`', '{', '|', '}', '~', '_', '«', '»']
+end_symbols = [',', '.', '!', '?']
+spaces = [' .', ' ,', ' !', ' ?']
+
+while True:
+    try:
+        file_str = input('Имя файла: ')
+        with open(file_str) as f_in:
+            text = f_in.read()
+    except FileNotFoundError:
+        print('Файл {} не найден'.format(file_str))
+        continue
+    break
+with open('output.txt', 'w', encoding="UTF-8") as f_out:
+    for i in text:
+        if i in list_of_symbols:
+            text = text.replace(i, '')
+    for j in spaces:
+        if j in text:
+            if j == ' .':
+                text = text.replace(j, '.')
+            elif j == ' ,':
+                text = text.replace(j, ',')
+            elif j == ' !':
+                text = text.replace(j, '!')
+            elif j == ' ?':
+                text = text.replace(j, '?')
+    f_out.write(text)
