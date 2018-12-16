c587e9979978f0de4165a18bbf4712fd0d0ae10b
arkroft <44421320+arkroft@users.noreply.github.com>
2018-12-16 23:01:24 +0700

    Add files via upload

--- case7.py
+++ case7.py
@@ -5,11 +5,33 @@ Selishchev A., Krivosheenkova E., Paymushkin K.,
 """
 
 import random
+from collections import defaultdict
 
 list_of_symbols = ['"', '#', '$', '%', '&', "'", '(', ')', '*',	'+', '-', '/', ':', ';', '<', '=', '>', '@', '[', ']',
-                   '^', '`', '{', '|', '}', '~', '_', '«', '»', '—']
+                   '^', '`', '{', '|', '}', '~', '_', '«', '»', '—', '–']
 end_symbols = [',', '.', '!', '?']
 spaces = [' .', ' ,', ' !', ' ?']
+
+def create_dict(ltxt):
+    d = defaultdict(list)
+    unique_word = []
+    for wrd in ltxt:
+        if wrd not in unique_word:
+            unique_word.append(wrd)
+    for wrd in unique_word:
+        for kk in range(len(ltxt) -1):
+            if ltxt[kk] == wrd:
+                stn = ltxt[kk + 1]
+                we = False
+                for ii in range(len(d[wrd])):
+                    if stn == d[wrd][ii]:
+                        we = True
+                        break
+                    else:
+                        we = False
+                if not we:
+                    d[wrd].append(stn)
+    return d
 
 while True:
     try:
@@ -20,6 +42,7 @@ while True:
         print('Файл {} не найден'.format(file_str))
         continue
     break
+
 for i in text:
     if i in list_of_symbols:
         text = text.replace(i, '')
@@ -33,3 +56,21 @@ for j in spaces:
             text = text.replace(j, '!')
         elif j == ' ?':
             text = text.replace(j, '?')
+
+#Делим текст на слова и заносим в список
+list_text = list(text.split())
+print(list_text)
+
+#Получаем "звенья и связи" и заносим в словарь функцией create_dict()
+dict_txt = create_dict(list_text)
+print(dict_txt)
+
+#Получаем список стартовых слов - начинающихся с заглавной буквы
+start_words = []
+for ch in list_text:
+    if ch == ch.capitalize():
+        if not ch in start_words:
+            start_words.append(ch)
+
+print(start_words)
+
